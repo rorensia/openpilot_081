eb769d1aa540aeb1f845554bd186941e21770cbe
Vehicle Researcher <>
2021-01-13 09:08:13 +0900

    openpilot v0.8.1

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -81,15 +81,15 @@ def create_clu11(packer, frame, bus, clu11, button, speed):
 def create_lfahda_mfc(packer, enabled, hda_set_speed=0):
   values = {
     #"LFA_USM": lfahda_mfc["LFA_USM"],
-    "LFA_USM": 2,
+    #"LFA_USM": 2,
     "LFA_Icon_State": 2 if enabled else 0,
     #"LFA_SysWarning": 0,
     #"HDA_USM": lfahda_mfc["HDA_USM"],
-    "HDA_USM": 2,
+    #"HDA_USM": 2,
     #"HDA_Active": 1 if hda_set_speed else 0,
-    "HDA_Active": 1 if enabled else 0,
-    #"HDA_Icon_State": 2 if hda_set_speed else 0,
-    "HDA_Icon_State": 2 if enabled else 0,
+    "HDA_Active": 1 if hda_set_speed else 0,
+    "HDA_Icon_State": 2 if hda_set_speed else 0,
+    #"HDA_Icon_State": 2 if enabled else 0,
     "HDA_VSetReq": hda_set_speed,
 
     #"HDA_USM": 2,
